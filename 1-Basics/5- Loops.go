5ac4e9384ba4231ed1f7dadcc94b68a5bad03e34
firat.atceken <firat.atceken@yemeksepeti.com>
2021-09-03 15:27:38 +0300

    Add switch, loops

---
+++ 1-Basics/5- Loops.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Println("-----------")
+
+	for i := 0; i < 10; i+=2 {
+		fmt.Println(i)
+	}
+
+
+	
+}
+
+
